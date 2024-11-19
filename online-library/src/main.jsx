0e9c128fded5b318056ccf66a5ca649375d352e6
Sapna44Y <yadavsapna2004@gmail.com>
2024-11-19 12:19:41 +0530

    changes main.jsx

--- online-library/src/main.jsx
+++ online-library/src/main.jsx
@@ -1,16 +1,33 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { Provider } from "react-redux";
+// import { BrowserRouter } from "react-router-dom";
+// import App from "./App";
+// import store from "./redux/store";
+// import "./index.css";
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Provider>,
+//   document.getElementById("root")
+// );
+
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; // Updated import for React 18
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import store from "./redux/store";
 import "./index.css";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
