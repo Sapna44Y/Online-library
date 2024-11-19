ee41d60ca8f66f98f8511e7bf8ee43a12bf8affa
Sapna44Y <yadavsapna2004@gmail.com>
2024-11-19 12:22:41 +0530

    same changes on main.jsx file

--- online-library/src/main.jsx
+++ online-library/src/main.jsx
@@ -1,20 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { Provider } from "react-redux";
-// import { BrowserRouter } from "react-router-dom";
-// import App from "./App";
-// import store from "./redux/store";
-// import "./index.css";
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </Provider>,
-//   document.getElementById("root")
-// );
-
 import React from "react";
 import ReactDOM from "react-dom/client"; // Updated import for React 18
 import { Provider } from "react-redux";
